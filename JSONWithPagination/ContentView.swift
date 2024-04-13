7b6ff36e815f5add56a715358a1b27303eff5545
Phakin <Phakhin.k@kkumail.com>
2024-04-13 11:03:25 +0700

    Initial Commit

---
+++ JSONWithPagination/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JSONWithPagination
+//
+//  Created by Phakhin Kanch on 13/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JSONWithPagination/JSONWithPaginationApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONWithPaginationApp.swift
+//  JSONWithPagination
+//
+//  Created by Phakhin Kanch on 13/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct JSONWithPaginationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
